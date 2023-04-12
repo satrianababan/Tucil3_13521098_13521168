64099fc55761863507b16dd2167462a8ad18c3c5
Satria Octavianus Nababan <88760287+satrianababan@users.noreply.github.com>
2023-04-12 09:17:47 +0700

    feat : astar

--- src/Astar.py
+++ src/Astar.py
@@ -1,89 +1,52 @@
-from math import *
+import heapq
+import math
 
-def haversine(dot1, dot2):
-    # jari2 bumi dalam km
-    r = 6371
-    dtr = pi/180
-    lat = (dot2["x"]-dot1["x"])*dtr
-    lon = (dot2["y"]-dot1["y"])*dtr
-    akar = sin(lat/2)**2 + cos(dot2["x"]*dtr) * cos(dot1["x"]*dtr) * sin(lon/2)**2
-    return 2*r*asin(sqrt(akar))
+class Graph:
+    def __init__(self, graph_dict=None):
+        if graph_dict is None:
+            graph_dict = {}
+        self.graph_dict = graph_dict
 
-def dheuristics(parsed, goal_node):
-    for i in range(len(parsed[1])):
-        if parsed[1][i] == goal_node:
-            end = i
-    dgoal = []
-    # Buat dictionary key = node, value = jarak lurus node ke simpul tujuan
-    h={}
-    for i in range(len(parsed[1])):
-        dgoal.append(haversine(parsed[2][end],parsed[2][i]))
-        h[parsed[1][i]] = dgoal[i]
-    return h
+    def neighbors(self, node):
+        return self.graph_dict.get(node, {}).keys()
 
-def sortB (list, dictB):
-    # Mengurutkan list simpul sesuai dengan Bobot value terminimum
-    dicttemp = {}
-    for node in list:
-        if dictB.get(node, "Not Available") != "Not Available":
-          dicttemp[node] = dictB[node]
-    dicttemp = dict(sorted(dicttemp.items(), key=lambda item: item[1]))
-    sorted_list = []
-    for i in dicttemp.keys():
-      sorted_list.append(i)
-    return sorted_list
+    def cost(self, node, next_node):
+        return self.graph_dict.get(node, {}).get(next_node, math.inf)
 
-# A* search
-def astar_search(parsed, heuristics, start_node, goal_node): 
-    
-    # Inisialisasi open node dan closed node
-    open_node = []
-    closed_node = []
+    def heuristic(self, node, end):
+        lat1, lon1 = node
+        lat2, lon2 = end
+        R = 6371  # jari-jari bumi dalam kilometer
+        phi1 = math.radians(lat1)
+        phi2 = math.radians(lat2)
+        delta_phi = math.radians(lat2 - lat1)
+        delta_lambda = math.radians(lon2 - lon1)
+        a = math.sin(delta_phi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        d = R * c
+        return d
 
-    # Bikin dictionary prev, key = node dan value = parent node
-    prev = {}
-    for i in parsed[1]:
-      prev[i] = None
+def astar(graph, start, end):
+    open_list = [(0, start)]
+    came_from = {}
+    g_score = {start: 0}
+    f_score = {start: graph.heuristic(start, end)}
 
-    # Bikin dictionary B value, D value
-    dictB= {}
-    dictB[start_node] = heuristics[start_node]
-
-    dictD = {}
-    dictD[start_node] = 0
-
-    # Append simpul awal ke list open node
-    open_node.append(start_node)
-    
-    # Looping open node hingga open node kosong
-    while len(open_node) > 0:
-        # Ambil simpul yang memiliki B value terkecil
-        open_node = sortB(open_node, dictB)
-        current_node = open_node.pop(0)
-        closed_node.append(current_node)
-
-        # Jika sudah mencapai simpul tujuan
-        if current_node == goal_node:
-            path = []
-            while current_node != start_node:
-                path.append(current_node)
-                current_node = prev[current_node]
-            path.append(start_node)
+    while open_list:
+        _, current = heapq.heappop(open_list)
+        if current == end:
+            path = [end]
+            while current in came_from:
+                current = came_from[current]
+                path.append(current)
             return path[::-1]
 
-        # looping simpul tetangga dari current node
-        neighbors = parsed[0][current_node]  
+        for neighbor in graph.neighbors(current):
+            tentative_g_score = g_score[current] + graph.cost(current, neighbor)
+            if tentative_g_score < g_score.get(neighbor, math.inf):
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = tentative_g_score + graph.heuristic(neighbor, end)
+                heapq.heappush(open_list, (f_score[neighbor], neighbor))
 
-        for neighbor in neighbors.keys():    
-            # Kasus simpul sudah diperiksa
-            if(neighbor in closed_node):
-                continue
-            prev[neighbor] = current_node
-
-            # Update nilai D value dan B value jika B value baru lebih minimum
-            if(dictD[current_node] + neighbors[neighbor] + heuristics[neighbor] < dictB.get(neighbor, 99999999)):
-              dictD[neighbor] = dictD[current_node] + neighbors[neighbor] 
-              dictB[neighbor] = dictD[neighbor] + heuristics[neighbor]
-              open_node.append(neighbor)
-    # Return None jika tidak ada jalur
     return None
