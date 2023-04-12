4b6dc7529806bbdbcff29f90af65203e5d2ed351
fazelginanda <fzginanda@gmail.com>
2023-04-12 12:30:08 +0700

    feat: add class LinkedNode

---
+++ src/LinkedNode.py
@@ -0,0 +1,29 @@
+from Node import *
+
+class LinkedNode(Node):
+    def __init__(self, name:int, coordinate: Coordinate, currentNode:Node, parentNode:Node, nodeCost=0):
+        super().__init__(name,coordinate)
+        self.__currentNode = currentNode
+        self.__parentNode = parentNode
+        self.__nodeCost = nodeCost
+
+    def getCurrentNode(self):
+        return self.__currentNode
+
+    def getParentNode(self) -> Node:
+        return self.__parentNode
+
+    def getNodeCost(self) -> float:
+        return self.__nodeCost
+
+    def setCurrentNode(self, currentNode):
+        self.__currentNode = currentNode
+
+    def setParentNode(self, parentNode):
+        self.__parentNode = parentNode
+
+    def setNodeCost(self, nodeCost):
+        self.__nodeCost = nodeCost
+
+    def __lt__(self, other):
+        return (self.getNodeCost() < other.getNodeCost())
